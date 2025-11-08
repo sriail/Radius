c4d02767f4c762f3fd84b4af492ed09e047a52d8
sriail <mackinleymorrone@gmail.com>
2025-11-08 13:51:35 -0500

    more search engines bc why not
    just more search options, also who will use any of these lol

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -12,9 +12,15 @@ type DropdownOptions = {
 };
 
 const SearchEngines: Record<string, string> = {
+    Aol: "https://search.aol.com/aol/search?q=%s",
+    Bing: "https://bing.com/search?q=%s",
+    Brave: "https://search.brave.com/search?q=%s",
     DuckDuckGo: "https://duckduckgo.com/?q=%s",
     Google: "https://google.com/search?q=%s",
-    Bing: "https://bing.com/search?q=%s"
+    Swisscows: "https://swisscows.com/en/web?query=%s",
+    Yahoo: "https://search.yahoo.com/search?p=%s",
+    Yandex: "https://yandex.com/search/?text=%s",
+    Quant: "https://www.qwant.com/?q=%s"
 };
 
 export { type SettingsProps, type DropdownOptions, SearchEngines };
